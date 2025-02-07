ed8dc8ea0bddf18ea029fcc8e646f2a39199e4a2
Rivky <rivka747343@gmail.com>
2025-02-07 12:58:43 +0200

    link to server cloud

--- practicud3Client-master/src/service.js
+++ practicud3Client-master/src/service.js
@@ -40,8 +40,8 @@
 //   }
 // };
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:5123/items";
-
+// axios.defaults.baseURL = "http://localhost:5123/items";
+axios.defaults.baseURL =process.env.REACT_APP_BASE_URL;
 axios.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
